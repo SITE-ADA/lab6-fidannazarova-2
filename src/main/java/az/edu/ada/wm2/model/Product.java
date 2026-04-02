b53276dfb1be163b9c8917b1f925e315c8b47870
fidannazarova <fnazarova15216@ada.edu.az>
2026-04-02 17:05:54 +0400

    Add ProductRepository

---
+++ src/main/java/az/edu/ada/wm2/model/Product.java
@@ -0,0 +1,42 @@
+package az.edu.ada.wm2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String productName;
+    private BigDecimal price;
+    private LocalDate expirationDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    @Builder.Default
+    private List<Category> categories = new ArrayList<>();
+}
---
+++ src/main/java/az/edu/ada/wm2/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package az.edu.ada.wm2.repository;
+
+import az.edu.ada.wm2.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByExpirationDateBefore(LocalDate date);
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+}
